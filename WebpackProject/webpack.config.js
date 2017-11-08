5e1664f9c585c3e8db4aefda26efd84530458fa9
Philip Damra <philip.damra@gstv.com>
2017-11-08 18:12:21 -0500

    Basic setup for webpack babel and css loaders

--- WebpackProject/webpack.config.js
+++ WebpackProject/webpack.config.js
@@ -2,9 +2,22 @@ var webpack = require('webpack');
 var path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js'
-  }
+	entry: './src/index.js',
+	output: {
+		path: path.join(__dirname, 'dist'),
+		filename: 'bundle.js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				use: 'babel-loader',
+				exclude: /node_modules/
+			},
+			{
+				test: /\.css$/,
+				use: ['style-loader', 'css-loader']
+			}
+		]
+	}
 };
