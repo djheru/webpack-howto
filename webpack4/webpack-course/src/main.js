8ec6f54cdc11a859b4383ab4877681c34b62939e
Philip Damra <djheru@gmail.com>
2018-06-07 00:48:27 -0400

    Set up react with hot reloading

--- webpack4/webpack-course/src/main.js
+++ webpack4/webpack-course/src/main.js
@@ -1,14 +1,7 @@
 require('babel-runtime/regenerator');
+require('react-hot-loader/patch');
+require('babel-register');
 require('webpack-hot-middleware/client?reload=true');
 require('./main.css');
 require('./index.html');
-debugger;
-console.log("Hello World");
-// alert("Wowza");
-var a = async (args) => {
-  const { a, b } = args;
-  await console.log('hello from the future', a, b);
-  console.log('Done!');
-};
-
-a({ a: 1, b: 'foo' });
+require('./app');
